b93170c28daa6c5ab9b6869e368801aab26b856e
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-13 09:41:05 +0100

    fix search icon && add books unique link

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,42 +1,11 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('app:bookRoutes');
 
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [{
-    title: 'Chronicles of Enigx',
-    genre: 'Historical fiction',
-    author: 'Ryan Scott',
-    read: true,
-  },
-  {
-    title: 'Planet of Rostian',
-    genre: 'Historical fiction',
-    author: 'Michael Scoffield',
-    read: false,
-  },
-  {
-    title: 'Right of Rignet',
-    genre: 'Historical fiction',
-    author: 'Dave Flisnsten',
-    read: false,
-  },
-  {
-    title: 'Then and Now',
-    genre: 'Drama',
-    author: 'Platonic',
-    read: true,
-  },
-  {
-    title: 'Mafia Beaurea',
-    genre: 'Historical fiction',
-    author: 'Blake Scott',
-    read: true,
-  },
-  ];
   bookRouter.route('/')
     .get((req, res) => {
       const url = 'mongodb://localhost:27017';
@@ -72,14 +41,32 @@ function router(nav) {
   bookRouter.route('/:id')
     .get((req, res) => {
       const { id } = req.params;
-      res.render(
-        'bookView',
-        {
-          nav,
-          title: 'Library',
-          book: books[id],
-        },
-      );
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to the server');
+
+          const db = client.db(dbName);
+
+          const col = await db.collection('books');
+
+          const book = await col.findOne({ _id: new ObjectID(id) });
+          debug(book);
+          res.render(
+            'bookView',
+            {
+              nav,
+              title: 'Library',
+              book,
+            },
+          );
+        } catch (err) {
+          debug(err.stack);
+        }
+      }());
     });
   return bookRouter;
 }
