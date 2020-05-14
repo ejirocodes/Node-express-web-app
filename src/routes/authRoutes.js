3a70cb2bb91393e117c500ca20e7c76c12c227d3
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-14 11:13:36 +0100

    create user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,10 +7,30 @@ const authRouter = express.Router();
 function router() {
   authRouter.route('/signUp')
     .post((req, res) => {
+      const { username, password } = req.body;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function addUser() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+
+          const db = client.db(dbName);
+
+          const col = db.collection('users');
+          const user = { username, password };
+          const results = await col.insertOne(user);
+          debug(results);
+          req.login(results.ops[0], () => {
+            res.redirect('/auth/profile');
+          });
+        } catch (err) {
+          debug(err);
+        }
+      }());
       debug(req.body);
-      req.login(req.body, () => {
-        res.redirect('/auth/profile');
-      });
     });
   authRouter.route('/profile')
     .get((req, res) => {
