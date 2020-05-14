8abb887e70a661f3597fba12c20fae916627d55b
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-05-14 21:25:37 +0100

    implement controllers

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,80 +1,19 @@
 const express = require('express');
 const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('app:bookRoutes');
+const bookController = require('../controllers/bookController');
 
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  bookRouter.use((req, res, next) => {
-    if (req.user) {
-      next();
-    } else {
-      res.redirect('/');
-    }
-  });
+  const { getIndex, getById, middleware } = bookController(nav);
+  bookRouter.use(middleware);
   bookRouter.route('/')
-    .get((req, res) => {
-      const url = 'mongodb://localhost:27017';
-      const dbName = 'libraryApp';
-
-      (async function mongo() {
-        let client;
-        try {
-          client = await MongoClient.connect(url);
-          debug('Connected correctly to the server');
-
-          const db = client.db(dbName);
-
-          const col = await db.collection('books');
-
-          const books = await col.find().toArray();
-
-          res.render(
-            'bookListView',
-            {
-              nav,
-              title: 'Library',
-              books,
-            },
-          );
-        } catch (err) {
-          debug(err.stack);
-        }
-        client.close();
-      }());
-    });
+    .get(getIndex);
 
   bookRouter.route('/:id')
-    .get((req, res) => {
-      const { id } = req.params;
-      const url = 'mongodb://localhost:27017';
-      const dbName = 'libraryApp';
-      (async function mongo() {
-        let client;
-        try {
-          client = await MongoClient.connect(url);
-          debug('Connected correctly to the server');
-
-          const db = client.db(dbName);
-
-          const col = await db.collection('books');
-
-          const book = await col.findOne({ _id: new ObjectID(id) });
-          debug(book);
-          res.render(
-            'bookView',
-            {
-              nav,
-              title: 'Library',
-              book,
-            },
-          );
-        } catch (err) {
-          debug(err.stack);
-        }
-      }());
-    });
+    .get(getById);
   return bookRouter;
 }
 
